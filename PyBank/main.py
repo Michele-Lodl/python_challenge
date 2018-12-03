e999bf92f320cd783f67fbe259e1a4382a417dd2
Clay Style <42677390+Michele-Lodl@users.noreply.github.com>
2018-12-03 17:07:18 -0600

    Printed Total Profit/Losses

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,5 @@
 # PyBank Homework. Analyze:
 
-# HW The total number of months included in the dataset
-
 #read_cvs.py class
 # First we'll import the os module. The os module means that we can create file paths across operating systems. 
 # This module is especially important if you want to make your programs platform-independent. os figures out which sysytem on and runs based on that
@@ -25,25 +23,40 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
   #Best practice to check to see if reader worked by printing. Will know by seeing "cvs.reader object at...blah, blah, blah. Then # out. 
-    print(csvreader)
+    # print(csvreader)
 
-    # Read the header row first (skip this step if there is header. Print it the header after the hard coded text CSV header.)
+    # Read the header row first (skip this step if there is no header. Print it the header after the hard coded text CSV header.)
     # HELP: don't really get next and what is happening here. 
-   #. csv_header = next(csvreader)
-   #. print(f"CSV Header: {csv_header}")
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
 
-    #. Read each row of data after the header and print them
-    #. HELP: do I have to call row, row? It is a method?
-    #.for row in csvreader:
-      #. if row[0] == 1
-      #.  print(row[0])
-      #.  else 
-     #.    print("No") 
 
-# The total net amount of "Profit/Losses" over the entire period
+    # Look for each row of data. Do something if the desired call is found; do somethign else if not
+    # Read each row of data , iteratate over something 
+    
+     
+   # The total net amount of "Profit/Losses" over the entire period
+
+    TotalPL = 0
+    # TotalDate = 0
+    for row in csvreader:
+      #This tells the computer to look at the first column for the variable I called "Date"
+      # Date = row[0]
+      # TotalMonths = TotalMonths + Date
+
+      #This tells the computer to look at the second column for the variable I called "Months_Profit_Losses" and casted as an integer 
+      Months_Profit_Losses = int(row[1])
+      # print(row[1])
+
+      TotalPL = TotalPL + Months_Profit_Losses
+    # print(TotalMonths)
+    print(TotalPL)
+
+print("The End")
+# The total number of months included in the dataset
+# DONE.The total net amount of "Proft/Losses" over the entire period
 # The average change in "Profit/Losses" between months over the entire period
 # The greatest increase in profits (date and amount) over the entire period
 # The greatest decrease in losses (date and amount) over the entire period
 # In addition, your final script should both print the analysis to the terminal and export a text file with the results.
 # WIP - both analysis to terminal?
-print("The End")
